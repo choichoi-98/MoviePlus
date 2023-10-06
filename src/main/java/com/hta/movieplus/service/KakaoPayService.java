dd5f45252fb94bd362e0d85a9ae8e2fc3dcdfe27
jaehyucklim <jhlim087@gmail.com>
2023-10-06 10:35:07 +0900

    store_admin con

--- src/main/java/com/hta/movieplus/service/KakaoPayService.java
+++ src/main/java/com/hta/movieplus/service/KakaoPayService.java
@@ -1,13 +1,6 @@
 package com.hta.movieplus.service;
 
-import com.hta.movieplus.domain.Member;
-
 public interface KakaoPayService {
 	
-	public int insert(Member m);
-	
-	public int isId(String MEMBER_ID, String MEMBER_PASS);
-	
-	public int isId(String MEMBER_ID);
 
 }
