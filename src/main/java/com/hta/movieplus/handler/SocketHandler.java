b07fb60245698562e61086fb42526a4e17c38f0e
heywon choi <choichoi980804@gmail.com>
2023-10-20 17:42:54 +0900

    choichoi

--- src/main/java/com/hta/movieplus/handler/SocketHandler.java
+++ src/main/java/com/hta/movieplus/handler/SocketHandler.java
@@ -2,20 +2,27 @@ package com.hta.movieplus.handler;
 
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
-@Component
+
+@Component  //SocketHandler 빈 등록
 public class SocketHandler extends TextWebSocketHandler{
 
+	private static final Logger logger = LoggerFactory.getLogger(SocketHandler.class);
+	
+	
 	HashMap<String, WebSocketSession> sessionMap = new HashMap<>(); //웹소켓 세션을 담아둘 맵
 	
 	@Override
 	public void handleTextMessage(WebSocketSession session, TextMessage message) {
 		//메시지 발송 - 메시지 수신 시 실행
+		
 		String msg = message.getPayload();
 		for(String key : sessionMap.keySet()) {
 			WebSocketSession wss = sessionMap.get(key);
@@ -30,7 +37,9 @@ public class SocketHandler extends TextWebSocketHandler{
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		//소켓 연결 - 웹소켓 연결 시 동작
+		logger.info("afterConnectionEstablished");
 		super.afterConnectionEstablished(session);
+		logger.info("afterConnection의 session" + session);
 		sessionMap.put(session.getId(), session);
 	}
 	
