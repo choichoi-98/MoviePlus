16fdf5d280306647d250eb06cad7ced52d7c7151
jhta <jhta@DESKTOP-GD0U3I1>
2023-10-24 09:36:19 +0900

    진행중

--- src/main/java/com/hta/movieplus/mybatis/mapper/CustomerMapper.java
+++ src/main/java/com/hta/movieplus/mybatis/mapper/CustomerMapper.java
@@ -6,13 +6,17 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Mapper;
 
 import com.hta.movieplus.domain.CustomerOneOnOneVO;
+import com.hta.movieplus.domain.NoticeVO;
 
 @Mapper /* class 만들어지면서 컨테이너에 등록이됨 */
 public interface CustomerMapper {
-	
+
 	public void insertCustomerOneOnOneVO(CustomerOneOnOneVO oneonone);
-	
-	/* public List<CustomerOneOnOneVO> getCustomerOneOnOneVOList();  일단 제외 */
-	
-	
+
+	/*
+	 * 공지사항 리스트 불러오기 public List<NoticeVO> getNoticeList(int NoticeNum);
+	 */
+
+	/* public List<CustomerOneOnOneVO> getCustomerOneOnOneVOList(); 일단 제외 */
+
 }
--- src/main/java/com/hta/movieplus/service/CustomerService.java
+++ src/main/java/com/hta/movieplus/service/CustomerService.java
@@ -3,10 +3,13 @@ package com.hta.movieplus.service;
 import java.util.List;
 
 import com.hta.movieplus.domain.CustomerOneOnOneVO;
+import com.hta.movieplus.domain.NoticeVO;
 
 public interface CustomerService {
 
 	public void insertOneOnOne(CustomerOneOnOneVO oneonone);
+
+	/* public List<NoticeVO> getNoticeList(int noticeNum); */
 
 	/* public List<CustomerOneOnOneVO> selectOneOnOne(); */
 
