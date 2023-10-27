4943fdee01b5496be7ba9fd6337f90c87df2eee5
jjsdev97 <jjjs963@naver.com>
2023-10-27 14:42:56 +0900

    오류 수정

--- src/main/java/com/hta/movieplus/controller/CustomerController.java
+++ src/main/java/com/hta/movieplus/controller/CustomerController.java
@@ -1,182 +1,109 @@
 package com.hta.movieplus.controller;
 
-import java.security.Principal;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.hta.movieplus.constant.TheaterLocationEnum;
+import com.hta.movieplus.domain.CustomerOneOnOneVO;
+import com.hta.movieplus.domain.Member;
 import com.hta.movieplus.domain.NoticeVO;
-import com.hta.movieplus.domain.TheaterRoom;
-import com.hta.movieplus.service.NoticeManagerService;
+import com.hta.movieplus.domain.Theater;
+import com.hta.movieplus.service.CustomerService;
 import com.hta.movieplus.service.NoticeService;
-import com.hta.movieplus.service.SeatService;
-import com.hta.movieplus.service.TheaterManagerService;
 import com.hta.movieplus.service.TheaterService;
 
 @Controller
-@RequestMapping(value = "/manager")
-@SessionAttributes({"theaterId", "theaterName"})
-public class ManagerController {
-	
-	TheaterManagerService theaterManagerService;
-	TheaterService theaterService;
-	SeatService seatService;
-	NoticeManagerService noticemanagerservice;
-	NoticeService noticeService;
-	
-	private static final Logger logger = LoggerFactory.getLogger(ManagerController.class);
+@RequestMapping(value = "/customer_service")
+public class CustomerController {
 
+	/* 컨트롤러 서비스 연결부분 */
+	CustomerService customerService;
+	NoticeService noticeservice;
+	TheaterService theaterservice;
+	
 
-	@Autowired
-	public ManagerController(TheaterManagerService theaterManagerService, TheaterService theaterService, SeatService seatService, NoticeManagerService noticemanagerservice, NoticeService noticeService) {
-		// TODO Auto-generated constructor stub
-		this.theaterManagerService = theaterManagerService;
-		this.theaterService = theaterService;
-		this.seatService = seatService;
-		this.noticemanagerservice = noticemanagerservice;
-		this.noticeService = noticeService;
+	@Autowired /* 생성자에 의존성주입 초기화 이미 만들어진객체를 new없이 받음 */
+	public CustomerController(CustomerService customerService, NoticeService noticeservice, TheaterService theaterservice) {
+		this.customerService = customerService;
+		this.noticeservice = noticeservice;
+		this.theaterservice = theaterservice;
+	}
+
+	private static final Logger logger = LoggerFactory.getLogger(CustomerController.class);
+	
+	//홈화면 이동 공지사항 전체 정보 가지고오기
+	@GetMapping("")
+	public String home(Model model) {
+		List<NoticeVO> totalnoticelist = noticeservice.getTotalNoticeList();
+		model.addAttribute("TotalNoticeList", totalnoticelist);
+		return "customer_service/customer_service_notice_total";
 	}
 	
-	@GetMapping("") 
-	public ModelAndView theaterMainView(ModelAndView mv, Principal principal, Model model,
-									@RequestParam(value="selectedId", required=false) String selectedId) {
-		
-		
-		String managerId = principal.getName();
-
-		if(selectedId != null) {
-			managerId = selectedId;
-		}
-		if(!managerId.equals("admin")) {
-			int theaterId = theaterManagerService.getTheaterIdByManagerId(managerId);
-			model.addAttribute("theaterId", theaterId);
-			model.addAttribute("theaterName", theaterService.getTheaterById(theaterId).getTHEATER_NAME());
-		} // 대쉬보드 재 입장 오류
-		
-		
-		mv.setViewName("manager/main");
-		return mv;
+	@GetMapping("/adminnotice")
+	public String adminNotice(Model model) {
+		List<NoticeVO> adminnotice = noticeservice.getNoticelist("전체");
+		model.addAttribute("AdminNotice" ,adminnotice);
+		return "customer_service/customer_service_notice_admin";
 	}
 	
-	@GetMapping("/manageroom")
-	public ModelAndView manageRoomView(ModelAndView mv, Model model) {
-		int theaterId = (int) model.asMap().get("theaterId");
-		int roomCount = theaterManagerService.getRoomCountById(theaterId);
-		List<TheaterRoom> roomList = theaterManagerService.getRoomList(theaterId);
-		
-		mv.addObject("roomCount", roomCount);
-		mv.addObject("roomList", roomList);
-		mv.setViewName("manager/manageRoom");
+	@GetMapping("/regionnotice")
+	public String regionNotice(Model model) {
+		List<NoticeVO> regionnotice = noticeservice.getNoticelistExceptEntire();
+		model.addAttribute("RegionNotice", regionnotice);
+		return "customer_service/customer_service_notice_region";
+	}
+	
+	@GetMapping("/oneonone")
+	public ModelAndView oneOnOne(ModelAndView mv) {
+		mv.addObject("locationList", TheaterLocationEnum.values());
+		mv.setViewName("customer_service/customer_service_oneonone");
 		return mv;
 	}
 
-	@GetMapping("/addroom")
-	public ModelAndView addRoomView(ModelAndView mv, Model model) {
-		int theaterId = (int) model.asMap().get("theaterId");
-		String roomName = theaterManagerService.getRoomName(theaterId);
-		
-		mv.addObject("roomName", roomName);
-		mv.setViewName("manager/addRoom");
-		
-		return mv;
+	@GetMapping("/myinjury")
+	public String myinjury(@AuthenticationPrincipal Member member, Model model) {
+		List<CustomerOneOnOneVO> myinjury = customerService.getMyInjury(member.getMEMBER_NUM());
+		model.addAttribute("MyInjury", myinjury);
+		return "customer_service/customer_service_myinjury"; // 리턴이 없어도되나요? 안되는거같음..
 	}
-	
-	@PostMapping("/addRoomAction")
-	public String addRoomAction(ModelAndView mv, TheaterRoom theaterRoom, Model model) {
-		int theaterId = (int) model.asMap().get("theaterId");
-		theaterRoom.setTHEATER_ID(theaterId);
 
-		theaterRoom.setTHEATER_ROOM_SEAT_CNT(seatService.getSeatCount(theaterRoom.getTHEATER_ROOM_SEAT()));
+	/* 1대1 문의 데이터 저장페이지 */
+	@PostMapping("/oneonone_process") //member는 MemberVO의 객체로 세션값을 가지고 있음 
+	public String oneProcess(@AuthenticationPrincipal Member member, CustomerOneOnOneVO oneonone) {
+		oneonone.setCUSTOMER_PHONNUM(
+				oneonone.getCUSTOMER_PHONNUM1() + oneonone.getCUSTOMER_PHONNUM2() + oneonone.getCUSTOMER_PHONNUM3());
 		
-		theaterManagerService.addTheaterRoom(theaterRoom);
+		oneonone.setMEMBER_NUM(member.getMEMBER_NUM());
+		customerService.insertOneOnOne(oneonone);
+		// 데이터는 컨트롤러에 있는데 서비스에 있는 도구를 쓰는거에요
+
+		return "redirect:/customer_service/myinjury";
+	}
+	
+	@ResponseBody
+	@PostMapping("/getTheaterListAjax")
+	public List<Theater> getTheaterListAjax(String location){
 		
-		
-		return "redirect:/manager/manageroom";
+		return theaterservice.getTheaterListByLocation(location);
 	}
 	
-	@GetMapping("/modifyRoom")
-	public ModelAndView modifyRoomView(int room_id, ModelAndView mv) {
-		TheaterRoom theaterRoom = theaterManagerService.getTheaterRoomById(room_id);
-		
-		mv.addObject("theaterRoom", theaterRoom);
-		mv.setViewName("manager/modifyRoom");
-		return mv;
+	@GetMapping("/myinjurydetail")
+	public String MyInjuryDetail(int detailnum, Model model) {
+		CustomerOneOnOneVO myinjurydetail = customerService.getDetailByNum(detailnum);
+		model.addAttribute("MyInjuryDetail",myinjurydetail);
+		return "customer_service/customer_service_myinjury_detail";
 	}
 	
-	@PostMapping("/modifyRoomAction")
-	public String modfiyRoomAction(TheaterRoom theaterRoom) {
-		
-		theaterRoom.setTHEATER_ROOM_SEAT_CNT(seatService.getSeatCount(theaterRoom.getTHEATER_ROOM_SEAT()));
-		theaterManagerService.modifyTheaterRoom(theaterRoom);
-		
 	
-		
-		return "redirect:/manager/manageroom";
-	}
-	
-	@GetMapping("/deleteRoomAction")
-	public String deleteRoomAction(int room_id) {
-		theaterManagerService.deleteRoomById(room_id);
-		return "redirect:/manager/manageroom";
-	}
-	
-	@GetMapping("/changeStatusRoomAction")
-	public String changeStatusRoomAction(int num, String status) {
-		theaterManagerService.changeStatusRoomById(num, status);
-		return "redirect:/manager/manageroom";
-	}
-	//---------------------------------------------------------------------------------//
-	/* 극장별 공지사항 main 페이지 리스트로 이동 */
-	@GetMapping("/noticelist")
-	public String getNoticeList(Model model) {
-		String theaterId = model.asMap().get("theaterId").toString();
-		List<NoticeVO> noticelist = noticemanagerservice.getNoticeList(theaterId);
-		model.addAttribute("NoticeList", noticelist);
-		return "manager/managerNoticeList";
-	}
-	
-	/* 극장별 공지사항작성 페이지 이동 */
-	@GetMapping("/noticeregist")
-	public String goNoticeWrite() {
-		return "manager/managerNotice";
-	}
-	
-	/* 공지사항 등록 버튼 클릭 -> 데이터 넘기기 */
-	@PostMapping("/insertnotice")
-	public String insertNotice(NoticeVO managernotice, Model model) {
-		String theaterId = model.asMap().get("theaterId").toString();
-		managernotice.setCUSTOMER_NOTICE_TYPE(theaterId);
-		noticemanagerservice.insertNotice(managernotice);
-		logger.info(managernotice.toString());
-		return "redirect:/manager/noticelist";
-	}
-	// 공지사항 수정페이지로 이동하면서 데이터 가져옴
-	@GetMapping("/gonoticemodify")
-		public String goManagerNoticeModify(int managernoticenum, Model model) {
-		NoticeVO managernotice = noticemanagerservice.getManagerNoticeModify(managernoticenum);
-		model.addAttribute("ManagerNotice", managernotice);
-		return "manager/managerNoticeModify";
-	}
-	
-	@PostMapping("/noticemodify")
-	public String noticeModify(NoticeVO modify) {
-		noticeService.noticeModify(modify);
-		return "redirect:/manager/noticelist";
-	}
-	
-	@GetMapping("/noticedelete")
-	public String noticedelete(int noticenum) {
-		noticeService.deleteNoticeVO(noticenum);
-		return "redirect:/manager/noticelist";
-	}
 }
